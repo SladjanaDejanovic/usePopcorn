2561be9194524d1a6a67bc8a0872fed7ef2e62d9
Sladjana <csl.bunny.2207@gmail.com>
2024-04-17 23:41:06 +0200

    rating component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useState } from "react";
 // import './index.css';
 // import App from './App';
 
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      className="test"
+    />
+    <StarRating color="red" size={20} defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
