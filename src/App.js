f50883ad97b7a4c2229ad5924bbaaa9e90a54631
Sladjana <csl.bunny.2207@gmail.com>
2024-04-26 22:24:58 +0200

    watched movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { func } from "prop-types";
 import { useEffect, useState } from "react";
 import StarRating from "./StarRating";
+import WatchedMovie from "./WatchedMovie";
 
 const tempMovieData = [
   {
@@ -395,31 +396,31 @@ function WatchedMoviesList({ watched, onDeleteWatched }) {
   );
 }
 
-function WatchedMovie({ movie, onDeleteWatched }) {
-  return (
-    <li>
-      <img src={movie.poster} alt={`${movie.title} poster`} />
-      <h3>{movie.title}</h3>
-      <div>
-        <p>
-          <span>⭐️</span>
-          <span>{movie.imdbRating}</span>
-        </p>
-        <p>
-          <span>🌟</span>
-          <span>{movie.userRating}</span>
-        </p>
-        <p>
-          <span>⏳</span>
-          <span>{movie.runtime} min</span>
-        </p>
-        <button
-          className="btn-delete"
-          onClick={() => onDeleteWatched(movie.imdbID)}
-        >
-          X
-        </button>
-      </div>
-    </li>
-  );
-}
+// function WatchedMovie({ movie, onDeleteWatched }) {
+//   return (
+//     <li>
+//       <img src={movie.poster} alt={`${movie.title} poster`} />
+//       <h3>{movie.title}</h3>
+//       <div>
+//         <p>
+//           <span>⭐️</span>
+//           <span>{movie.imdbRating}</span>
+//         </p>
+//         <p>
+//           <span>🌟</span>
+//           <span>{movie.userRating}</span>
+//         </p>
+//         <p>
+//           <span>⏳</span>
+//           <span>{movie.runtime} min</span>
+//         </p>
+//         <button
+//           className="btn-delete"
+//           onClick={() => onDeleteWatched(movie.imdbID)}
+//         >
+//           X
+//         </button>
+//       </div>
+//     </li>
+//   );
+// }
