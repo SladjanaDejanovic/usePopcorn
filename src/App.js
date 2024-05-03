a4ac9358315b2cdd81281ac370e1e550412be644
Sladjana Dejanovic <109485412+SladjanaDejanovic@users.noreply.github.com>
2024-05-03 13:34:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import { useKey } from "./useKey";
 //   {
 //     imdbID: "tt1375666",
 //     Title: "Inception",
-//     Year: "2010",
 //       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
 //   },
 //   {
