54eab056c8fc769685c61cd493f0fb0f54fd9a40
Sladjana <csl.bunny.2207@gmail.com>
2024-05-06 16:01:52 +0200

    update

--- src/App.js
+++ src/App.js
@@ -16,13 +16,7 @@ import { useKey } from "./useKey";
 //     Poster:
 //       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
 //   },
-//   {
-//     imdbID: "tt0133093",
-//     Title: "The Matrix",
-//     Year: "1999",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-//   },
+
 //   {
 //     imdbID: "tt6751668",
 //     Title: "Parasite",
