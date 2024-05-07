1545d2992fd4266e27cbd3fafb0fe1a8298b726d
Sladjana <csl.bunny.2207@gmail.com>
2024-05-07 17:10:57 +0200

    deleting comments

--- src/App.js
+++ src/App.js
@@ -8,20 +8,6 @@ import { useMovies } from "./useMovies";
 import { useLocalStorageState } from "./useLocalStorageState";
 import { useKey } from "./useKey";
 
-// const tempMovieData = [
-//     imdbID: "tt1375666",
-//       "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-//   },
-
-//   {
-//     imdbID: "tt6751668",
-//     Title: "Parasite",
-//     Year: "2019",
-//     Poster:
-//       "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-//   },
-// ];
-
 // const tempWatchedData = [
 //   {
 //     imdbID: "tt1375666",
